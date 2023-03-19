5f96fba257c2400eb455c633058236da7d11f943
Ilya Malinowski <i.v.malinovskiy@tinkoff.ru>
2023-03-19 13:31:44 +0300

    grayscale done

---
+++ src/main/kotlin/com/malinowski/GrayScale.kt
@@ -0,0 +1,51 @@
+package com.malinowski
+
+import kotlinx.coroutines.*
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.FileNotFoundException
+import javax.imageio.ImageIO
+import kotlin.coroutines.CoroutineContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GrayScale : CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Job() + Dispatchers.IO.limitedParallelism(10)
+    // new thread for grayscale with limited 10 threads
+
+    private val defPath = System.getProperty("user.dir")
+
+    fun grayscale(path: String) {
+        launch { // launch new coroutine for task
+            val file = File(path)
+            if (!file.exists()) {
+                throw FileNotFoundException()
+            }
+
+            // will complete in new coroutine
+            val origin = withContext(Dispatchers.IO) {
+                ImageIO.read(file)
+            }
+            val blackAndWhite = BufferedImage(origin.width, origin.height, BufferedImage.TYPE_BYTE_GRAY)
+            val graphics2D = blackAndWhite.createGraphics()
+            graphics2D.drawImage(origin, 0, 0, null)
+
+            // will complete in new coroutine
+            withContext(Dispatchers.IO) {
+                ImageIO.write(
+                    blackAndWhite, "png",
+                    File("$defPath/upload_grayscale/${file.name}")
+                )
+            }
+            println("RASPBERRY saved $defPath/upload_grayscale/${file.name}")
+        }
+    }
+
+    companion object {
+        private val impl by lazy {
+            GrayScale()
+        }
+
+        fun invoke() = impl
+    }
+}
--- src/main/kotlin/com/malinowski/plugins/Routing.kt
+++ src/main/kotlin/com/malinowski/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.malinowski.plugins
 
+import com.malinowski.GrayScale
 import io.ktor.http.*
 import io.ktor.http.ContentDisposition.Companion.File
 import io.ktor.http.content.*
@@ -7,54 +8,49 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.delay
 import java.io.File
+
+private val path  = System.getProperty("user.dir")
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
-        get("/web_services/grayscale") {
-            call.respondText("It's a service to grayscale an image!")
-
-        }
         get("/web_services/array") {
             call.respondText("It's a service to find the words containing a letter in an array!")
         }
-        post("/upload") { _ ->
-            // retrieve all multipart data (suspending)
+        post("/grayscale/upload") { _ ->
             val multipart = call.receiveMultipart()
             multipart.forEachPart { part ->
-                // if part is a file (could be form item)
                 if(part is PartData.FileItem) {
-                    // retrieve file name of upload
                     val name = part.originalFileName!!
-                    val path  = System.getProperty("user.dir")
                     val file = File("$path/upload_original/$name")
-                    print("RASPBERRY saved ${file.absolutePath}")
-                    // use InputStream from part to save file
+                    println("RASPBERRY saved ${file.absolutePath}")
                     part.streamProvider().use { its ->
-                        // copy the stream to the file with buffering
                         file.outputStream().buffered().use {
-                            // note that this is blocking
                             its.copyTo(it)
                         }
                     }
+                    // launch grayscale
+                    GrayScale().grayscale(file.absolutePath)
                 }
-                // make sure to dispose of the part after use to prevent leaks
                 part.dispose()
             }
         }
-        get("/download/{name}") {
-            // get filename from request url
+        get("/grayscale/download/{name}") {
             val filename = call.parameters["name"]!!
-            // construct reference to file
-            // ideally this would use a different filename
-            val file = File("/upload_grayscale/$filename")
-            if(file.exists()) {
-                call.respondFile(file)
+            val file = File("$path/upload_grayscale/$filename")
+            repeat(10) {
+                println("RASPBERRY file exists ${file.name} ${file.exists()}")
+                if(file.exists()) {
+                    call.respondFile(file)
+                    return@get
+                }
+                delay(1000) // wait for converting
             }
-            else call.respond(HttpStatusCode.NotFound)
+            call.respond(HttpStatusCode.NotFound)
         }
 
     }
